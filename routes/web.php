a6849bd6ba0a945744eb447903b48006417059e2
ft <>
2021-10-08 21:26:44 +0700

    Menyelesaikan video 3 laravel

---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('Home');
+});
+
+Route::get('/about', function () {
+    return view('About', [
+        "name" => "Nur Safitri Wulandari",
+        "email" => "193040181@mail.unpas.ac.id",
+        "image" => "profile.png"
+    ]);
+});
+
+Route::get('/blog', function () {
+    return view('posts');
+});
