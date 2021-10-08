fa5570b411425ff58b7e2dd07d64389346f864bd
ft <>
2021-10-08 23:14:49 +0700

    menyelesaikan video 4 laravel

--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('Home');
+    return view('Home', [
+        "title" => "Home"
+    ]);
 });
 
 Route::get('/about', function () {
     return view('About', [
+        "title" => "About",
         "name" => "Nur Safitri Wulandari",
         "email" => "193040181@mail.unpas.ac.id",
         "image" => "profile.png"
@@ -26,5 +29,51 @@ Route::get('/about', function () {
 });
 
 Route::get('/blog', function () {
-    return view('posts');
+    $blog_posts = [
+        [
+            "title" => "The Feels",
+            "slug" => "judul-post-pertama",
+            "author" => "Nur Safitri Wulandari",
+            "body" => "TWICE english single The Feels, Released October 1, 2021."
+        ],
+        [
+            "title" => "Formula of Love O+T=<3",
+            "slug" => "judul-post-kedua",
+            "author" => "Nur Safitri Wulandari",
+            "body" => "TWICE 3rd Full Album. Formula of Love O+T=<3. Release on November 12, 2021"
+        ],
+    ];
+    return view('posts', [
+        "title" => "Posts",
+        "posts" => $blog_posts
+    ]);
+});
+
+//halaman single post
+Route::get('posts/{slug}', function ($slug) {
+    $blog_posts = [
+        [
+            "title" => "The Feels",
+            "slug" => "judul-post-pertama",
+            "author" => "Nur Safitri Wulandari",
+            "body" => "TWICE english single The Feels, Released October 1, 2021."
+        ],
+        [
+            "title" => "Formula of Love O+T=<3",
+            "slug" => "judul-post-kedua",
+            "author" => "Nur Safitri Wulandari",
+            "body" => "TWICE 3rd Full Album. Formula of Love O+T=<3. Release on November 12, 2021"
+        ],
+    ];
+
+    $new_post = [];
+    foreach ($blog_posts as $post) {
+        if ($post["slug"] === $slug) {
+            $new_post = $post;
+        }
+    }
+    return view('post', [
+        "title" => "Single Post",
+        "post" => $new_post
+    ]);
 });
