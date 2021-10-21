84d49cb9467da56cea0d61472c05b413a760e8ce
ft <>
2021-10-22 06:35:57 +0700

    Menyelesaikan video 7 laravel

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -15,11 +15,11 @@ class PostController extends Controller
         ]);
     }
 
-    public function show($id)
+    public function show(Post $post)
     {
         return view('post', [
             "title" => "Single Post",
-            "post" => Post::find($id)
+            "post" => $post
         ]);
     }
 }
