7289f0eaf45864b6709f100f77b4e1aca4acd746
ft <>
2021-10-22 07:20:48 +0700

    Menyelesaikan video 8 laravel

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -3,6 +3,8 @@
 @section('container')
     <article>
       <h2>{{ $post->title }}</h2>
+
+    <p>By. Nur Safitri Wulandari in <a href="/categories/{{ $post->category->slug }}"></a> {{ $post->category->name }}</p>
       {!! $post->body !!}
     </article>
     <a href="/blog">Back to Posts</a>
