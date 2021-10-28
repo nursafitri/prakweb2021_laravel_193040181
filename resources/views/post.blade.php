adcc00c8a30d8785ea4fae0dda8d0979a84164f6
ft <>
2021-10-29 04:51:11 +0700

    Menyelesaikan video 9 laravel

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -4,8 +4,8 @@
     <article>
       <h2>{{ $post->title }}</h2>
 
-    <p>By. Nur Safitri Wulandari in <a href="/categories/{{ $post->category->slug }}"></a> {{ $post->category->name }}</p>
+    <p>By. <a href="#" class="text-decoration-none">{{ $post->user->name }}</a><a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a> </p>
       {!! $post->body !!}
     </article>
-    <a href="/blog">Back to Posts</a>
+    <a href="/posts" class="d-block mt-4">Back to Posts</a>
 @endsection
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -3,14 +3,16 @@
 @section('container')
 
 @foreach ($posts as $post)
-<article class="mb-5">
+<article class="mb-5 border-bottom">
 <h2>
-  <a href="/posts/{{ $post->slug }}">{{ $post->title }}</a>
+  <a href="/posts/{{ $post->slug }}" class="text-decoration-none">{{ $post->title }}</a>
+  <p>By. <a href="#" class="text-decoration-none">{{ $post->user->name }}</a><a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></p>
 </h2>
 <p>{{ $post->excerpt }}</p>
 
 <a href="/posts">Back to posts</a>
 
+<a href="/posts/{{ $post->slug }}">Read more...</a>
 </article>
 
 @endforeachs
