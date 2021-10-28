2e6e37edd778f281109164072933dde1f058edcf
ft <>
2021-10-29 06:15:49 +0700

    Menyelesaikan video 11 laravel

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,11 +1,13 @@
 @extends('layouts.main')
   
 @section('container')
+<h1 class="mb-5">{{ $tittle }}</h1>
 
 @foreach ($posts as $post)
 <article class="mb-5 border-bottom">
 <h2>
   <a href="/authors/{{ $post->author->username }}" class="text-decoration-none">{{ $post->title }}</a>
+
   <p>By. <a href="#" class="text-decoration-none">{{ $post->author->name }}</a><a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></p>
 </h2>
 <p>{{ $post->excerpt }}</p>
