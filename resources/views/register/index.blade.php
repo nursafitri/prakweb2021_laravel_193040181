c25bb955d812e025b751b11b2ba583f26b5df048
ft <>
2021-10-29 23:33:34 +0700

    Menyelesaikan video 16 laravel

--- resources/views/register/index.blade.php
+++ resources/views/register/index.blade.php
@@ -6,7 +6,7 @@
         <main class="form-registration">
           <h1 class="h3 mb-3 fw-normal text-center">Registration Form</h1>
           <form action="/register" method="post">
-            @csrf
+           
             <div class="form-floating">
               <input type="text" name="name" class="form-control rounded-top @error('name') is-invalid @enderror" id="name" placeholder="Name" required value="{{ old('name') }}">
               <label for="name">Name</label>
